bfe0ebad88b420c0e01faaa515192dc5717e585c
Akarit <AKaritMann244479@gmail.com>
2022-06-13 17:02:19 +0630

    schoolweb_singlepage_pure

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+console.log('work')
+const app = Vue.createApp({
+    data() {
+      return {
+        SC_SH1:false,
+        SC_SH2:false,
+        SC_SH3:false,
+        SC_SH4:false,
+        SC_SH5:false,
+        SC_SH6:false,
+      }
+    },
+  });
+  
+  
+  app.mount('#app')
+
+
+
